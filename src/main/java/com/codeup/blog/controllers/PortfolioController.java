b2b625e5a9f7dcdc467877d2731e91cb656c868a
Your Name <dorian.l.wallace@gmail.com>
2019-03-07 18:56:47 -0500

    fixed typo in img paths

--- src/main/java/com/codeup/blog/controllers/PortfolioController.java
+++ src/main/java/com/codeup/blog/controllers/PortfolioController.java
@@ -43,7 +43,7 @@ public class PortfolioController {
         model.addAttribute("portfolio", new Portfolio());
         return "create-portfolio-single";
     }
-@Value("${/srv/dorianlwallace.com/public}")
+@Value("/srv/dorianlwallace.com/public")
 private String uploadPort;
 
     @PostMapping("/portfolio/create")
