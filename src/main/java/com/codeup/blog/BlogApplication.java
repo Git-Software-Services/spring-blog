453362d6505150a87fff71146d878b6a0a2921c0
Your Name <dorian.l.wallace@gmail.com>
2019-03-07 13:56:32 -0500

    working on adding email cntroller

--- src/main/java/com/codeup/blog/BlogApplication.java
+++ src/main/java/com/codeup/blog/BlogApplication.java
@@ -1,11 +1,7 @@
 package com.codeup.blog;
 
-import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlogApplication {
